b99cf8b2ca25b351f7974c437b011a797c32dff6
trideptrai <lehuynhminhtri09082003@gmail.com>
2024-10-20 15:57:57 +0700

    Add loading

--- src/app/components/header/page.tsx
+++ src/app/components/header/page.tsx
@@ -8,6 +8,7 @@ import { usePathname } from "next/navigation";
 import { jwtDecode } from "jwt-decode";
 import axios from "axios";
 import "animate.css";
+import {  Spin } from "antd";
 
 interface DecodedToken {
   id: string;
@@ -25,6 +26,7 @@ const Header = () => {
   const [hasHydrated, setHasHydrated] = useState(false);
   const [name, setName] = useState<string | null>(null);
   const [role, setRole] = useState<string | null>(null);
+  const [spinning, setSpinning] = useState(false);
 
   const controlHeader = () => {
     if (typeof window !== "undefined") {
@@ -86,24 +88,44 @@ const Header = () => {
   }, []);
 
   const handleClick = (path: string) => {
-    router.push(path);
+    setSpinning(true);
+    setTimeout(() => {
+      router.push(path);
+      setSpinning(false);
+    }, 1000); // Simulate a delay for loading
   };
 
   const handleLoginClick = () => {
-    router.push("/signin");
+    setSpinning(true);
+    setTimeout(() => {
+      router.push("/signin");
+      setSpinning(false);
+    }, 1000); // Simulate a delay for loading
   };
 
   const handleLogoutClick = () => {
-    dispatch(logout());
-    router.push("/signin");
+    setSpinning(true);
+    setTimeout(() => {
+      dispatch(logout());
+      router.push("/signin");
+      setSpinning(false);
+    }, 1000); // Simulate a delay for loading
   };
 
   const handleAdminClick = () => {
-    router.push("/admin");
+    setSpinning(true);
+    setTimeout(() => {
+      router.push("/admin");
+      setSpinning(false);
+    }, 1000); // Simulate a delay for loading
   };
 
   const handleShelterStaffClick = () => {
-    router.push("/shelter-staff");
+    setSpinning(true);
+    setTimeout(() => {
+      router.push("/shelter-staff");
+      setSpinning(false);
+    }, 1000); // Simulate a delay for loading
   };
 
   if (!hasHydrated) {
@@ -111,158 +133,167 @@ const Header = () => {
   }
 
   return (
-    <div className="fixed z-50 w-full">
-      <div
-        className={`h-[42px] bg-[#FFCC00] flex items-center justify-between fixed top-0 left-0 right-0 z-40 transition-transform duration-300 ease-in-out animate__animated ${
-          showTopBar ? "animate__fadeInDown" : "animate__fadeOutUp"
-        }`}
-      >
-        <ul className="flex gap-4 sm:gap-10 pl-4 sm:pl-96 items-center">
-          <li className="flex items-center gap-2 group">
-            <Image src="/images/location.png" alt="" width={30} height={30} />
-            <div className="text-white group-hover:text-black transition duration-300">
-              SaiGon-VietNam
-            </div>
-          </li>
-          <li className="flex items-center gap-2 group">
-            <Image src="/images/mail.png" alt="" width={30} height={30} />
-            <div className="text-white group-hover:text-black transition duration-300">
-              trilhmse173578@fpt.edu.vn
-            </div>
-          </li>
-          <li className="flex items-center gap-2 group">
-            <Image src="/images/telephone.png" alt="" width={30} height={30} />
-            <div className="text-white group-hover:text-black transition duration-300">
-              (84+)5835484
-            </div>
-          </li>
-        </ul>
-        <ul className="flex gap-4 sm:gap-10 pr-4 sm:pr-60 items-center">
-          <li>
-            <Image src="/images/search.png" alt="" width={30} height={30} />
-          </li>
-          <li>
-            <Image src="/images/vietnam.png" alt="" width={30} height={30} />
-          </li>
-          <li className="animate__animated animate__fadeInRight">Hi {name ? name : "Guest"} ❤️</li>
-          <li>
-            <button
-              onClick={token ? handleLogoutClick : handleLoginClick}
-              className="rounded-md border border-black px-4 text-white bg-[#D94E66] hover:bg-white hover:text-[#D94E66] transition duration-300 animate__animated animate__fadeInRight"
-            >
-              {token ? "Logout" : "Login"}
-            </button>
-          </li>
-        </ul>
-      </div>
+    <Spin spinning={spinning} size="large">
+      <div className="fixed z-50 w-full">
+        <div
+          className={`h-[42px] bg-[#FFCC00] flex items-center justify-between fixed top-0 left-0 right-0 z-40 transition-transform duration-300 ease-in-out animate__animated ${
+            showTopBar ? "animate__fadeInDown" : "animate__fadeOutUp"
+          }`}
+        >
+          <ul className="flex gap-4 sm:gap-10 pl-4 sm:pl-96 items-center">
+            <li className="flex items-center gap-2 group">
+              <Image src="/images/location.png" alt="" width={30} height={30} />
+              <div className="text-white group-hover:text-black transition duration-300">
+                SaiGon-VietNam
+              </div>
+            </li>
+            <li className="flex items-center gap-2 group">
+              <Image src="/images/mail.png" alt="" width={30} height={30} />
+              <div className="text-white group-hover:text-black transition duration-300">
+                trilhmse173578@fpt.edu.vn
+              </div>
+            </li>
+            <li className="flex items-center gap-2 group">
+              <Image
+                src="/images/telephone.png"
+                alt=""
+                width={30}
+                height={30}
+              />
+              <div className="text-white group-hover:text-black transition duration-300">
+                (84+)5835484
+              </div>
+            </li>
+          </ul>
+          <ul className="flex gap-4 sm:gap-10 pr-4 sm:pr-60 items-center">
+            <li>
+              <Image src="/images/search.png" alt="" width={30} height={30} />
+            </li>
+            <li>
+              <Image src="/images/vietnam.png" alt="" width={30} height={30} />
+            </li>
+            <li className="animate__animated animate__fadeInRight">
+              Hi {name ? name : "Guest"} ❤️
+            </li>
+            <li>
+              <button
+                onClick={token ? handleLogoutClick : handleLoginClick}
+                className="rounded-md border border-black px-4 text-white bg-[#D94E66] hover:bg-white hover:text-[#D94E66] transition duration-300 animate__animated animate__fadeInRight"
+              >
+                {token ? "Logout" : "Login"}
+              </button>
+            </li>
+          </ul>
+        </div>
 
-      <div
-        className={`h-[106px] bg-[#F4F4F4] flex items-center justify-center fixed top-0 left-0 right-0 z-30 transition-transform duration-300 ease-in-out animate__animated ${
-          showTopBar ? "animate__fadeInDown mt-[42px]" : ""
-        }`}
-      >
-        <ul className="flex flex-wrap gap-4 sm:gap-[60px] items-center justify-center">
-          <li>
-            <Image
-              src="/images/logo.png"
-              alt="Logo"
-              width={106}
-              height={106}
-              className="animate__animated animate__bounceIn"
-            />
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={() => handleClick("/")}
-          >
-            HOMEPAGE
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/adopt"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={() => handleClick("/adopt")}
-          >
-            ADOPT
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/donate"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={() => handleClick("/donate")}
-          >
-            DONATE
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/volunteer"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={() => handleClick("/volunteer")}
-          >
-            VOLUNTEER
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/news"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={() => handleClick("/news")}
-          >
-            NEWS
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/contact"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={() => handleClick("/contact")}
-          >
-            CONTACT
-          </li>
-          <li
-            className={`cursor-pointer animate__animated animate__fadeInLeft ${
-              pathname === "/admin"
-                ? "text-[#D94E66]"
-                : "text-black hover:text-[#D94E66]"
-            }`}
-            onClick={
-              role === "ADMIN"
-                ? handleAdminClick
-                : role === "SHELTER_STAFF"
-                ? handleShelterStaffClick
-                : undefined
-            }
-          >
-            {role === "ADMIN" && "ADMIN DASHBOARD"}
-            {role === "SHELTER_STAFF" && "SHELTERSTAFF DASHBOARD"}
-          </li>
-          {role === "ADMIN" && (
+        <div
+          className={`h-[106px] bg-[#F4F4F4] flex items-center justify-center fixed top-0 left-0 right-0 z-30 transition-transform duration-300 ease-in-out animate__animated ${
+            showTopBar ? "animate__fadeInDown mt-[42px]" : ""
+          }`}
+        >
+          <ul className="flex flex-wrap gap-4 sm:gap-[60px] items-center justify-center">
+            <li>
+              <Image
+                src="/images/logo.png"
+                alt="Logo"
+                width={106}
+                height={106}
+                className="animate__animated animate__bounceIn"
+              />
+            </li>
             <li
               className={`cursor-pointer animate__animated animate__fadeInLeft ${
-                pathname === "/user"
+                pathname === "/"
                   ? "text-[#D94E66]"
                   : "text-black hover:text-[#D94E66]"
               }`}
-              onClick={() => handleClick("/user")}
+              onClick={() => handleClick("/")}
             >
-              USER
+              HOMEPAGE
             </li>
-          )}
-        </ul>
+            <li
+              className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                pathname === "/adopt"
+                  ? "text-[#D94E66]"
+                  : "text-black hover:text-[#D94E66]"
+              }`}
+              onClick={() => handleClick("/adopt")}
+            >
+              ADOPT
+            </li>
+            <li
+              className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                pathname === "/donate"
+                  ? "text-[#D94E66]"
+                  : "text-black hover:text-[#D94E66]"
+              }`}
+              onClick={() => handleClick("/donate")}
+            >
+              DONATE
+            </li>
+            <li
+              className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                pathname === "/volunteer"
+                  ? "text-[#D94E66]"
+                  : "text-black hover:text-[#D94E66]"
+              }`}
+              onClick={() => handleClick("/volunteer")}
+            >
+              VOLUNTEER
+            </li>
+            <li
+              className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                pathname === "/news"
+                  ? "text-[#D94E66]"
+                  : "text-black hover:text-[#D94E66]"
+              }`}
+              onClick={() => handleClick("/news")}
+            >
+              NEWS
+            </li>
+            <li
+              className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                pathname === "/contact"
+                  ? "text-[#D94E66]"
+                  : "text-black hover:text-[#D94E66]"
+              }`}
+              onClick={() => handleClick("/contact")}
+            >
+              CONTACT
+            </li>
+            <li
+              className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                pathname === "/admin"
+                  ? "text-[#D94E66]"
+                  : "text-black hover:text-[#D94E66]"
+              }`}
+              onClick={
+                role === "ADMIN"
+                  ? handleAdminClick
+                  : role === "SHELTER_STAFF"
+                  ? handleShelterStaffClick
+                  : undefined
+              }
+            >
+              {role === "ADMIN" && "ADMIN DASHBOARD"}
+              {role === "SHELTER_STAFF" && "SHELTERSTAFF DASHBOARD"}
+            </li>
+            {role === "ADMIN" && (
+              <li
+                className={`cursor-pointer animate__animated animate__fadeInLeft ${
+                  pathname === "/user"
+                    ? "text-[#D94E66]"
+                    : "text-black hover:text-[#D94E66]"
+                }`}
+                onClick={() => handleClick("/user")}
+              >
+                USER
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Spin>
   );
 };
 
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,11 +1,12 @@
-'use client';
+"use client";
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { Input } from "antd";
-import { useAppDispatch, useAppSelector } from '../../lib/hook';
-import { signin } from '../../lib/features/auth/authSlice';
-import { useRouter } from 'next/navigation';
-import 'animate.css';
+import { useAppDispatch, useAppSelector } from "../../lib/hook";
+import { signin } from "../../lib/features/auth/authSlice";
+import { useRouter } from "next/navigation";
+import "animate.css";
+import { Spin } from "antd";
 
 const SignIn = () => {
   const dispatch = useAppDispatch();
@@ -13,10 +14,11 @@ const SignIn = () => {
   const error = useAppSelector((state) => state.auth.error);
   const router = useRouter();
 
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [emailError, setEmailError] = useState('');
-  const [passwordError, setPasswordError] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const validateEmail = (email: string) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -45,101 +47,130 @@ const SignIn = () => {
     setPasswordError(passwordValidationError);
 
     if (!emailValidationError && !passwordValidationError) {
+      setLoading(true);
       dispatch(signin({ email, password }));
     }
   };
 
+  const handleSignup = () => {
+    setLoading(true);
+    router.push("/signup");
+  };
+
   useEffect(() => {
-    if (authStatus === 'succeeded') {
-      router.push('/home');  // Điều hướng sau khi đăng nhập thành công
+    if (authStatus === "succeeded") {
+      router.push("/home");
     }
+    setLoading(false);
   }, [authStatus, router]);
 
   return (
-    <div className="flex h-screen text-center justify-center align-middle animate__animated animate__fadeIn">
-      <div className="w-[50%] bg-gray-200 animate__animated animate__fadeInLeft">
-        <Image
-          src="/images/btlogin.png"
-          alt="Dog and Cat"
-          width={1000}
-          height={1000}
-          className="w-[100%] h-[100%]"
-        />
-      </div>
-      <div className="w-[50%] flex flex-col items-center justify-center animate__animated animate__fadeInRight pb-72">
-        <div className="">
-          <div className=" pt-28  w-[15%] h-[15%] animate__animated animate__bounceIn ">
-            <Image
-              src="/images/logo1.png"
-              alt="Logo"
-              width={1000}
-              height={1000}
-              className="object-contain"
-            />
-          </div>
+    <Spin spinning={loading} size="large">
+      <div className="flex h-screen text-center justify-center align-middle animate__animated animate__fadeIn">
+        <div className="w-[50%] bg-gray-200 animate__animated animate__fadeInLeft">
+          <Image
+            src="/images/btlogin.png"
+            alt="Dog and Cat"
+            width={1000}
+            height={1000}
+            className="w-[100%] h-[100%]"
+          />
         </div>
-        <div className="flex flex-col items-center">
-          <h1 className="text-[40px] font-bold pt-[140px] animate__animated animate__fadeInDown">Sign In</h1>
-          <h2 className="text-[35px] font-bold pt-5 animate__animated animate__fadeInDown animate__delay-1s">Welcome to PawFund</h2>
-          {authStatus === 'failed' && <p className="text-red-500 animate__animated animate__shakeX">{error}</p>} {/* Hiển thị lỗi nếu có */}
-          <div className="w-[50%] pt-[50px]">
-            <Input 
-              className="h-[40px] animate__animated animate__fadeInUp"
-              placeholder="Email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              style={{ backgroundColor: "#e5e4e4", border: "none" }}
-            />
-            {emailError && <p className="text-red-500">{emailError}</p>}
-            <Input.Password 
-              type="password"
-              className="h-[40px] animate__animated animate__fadeInUp animate__delay-1s"
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              style={{ backgroundColor: "#e5e4e4", border: "none", marginTop: "30px" }}
-            />
-            {passwordError && <p className="text-red-500">{passwordError}</p>}
-            <button onClick={handleSignin} className="font-semibold duration-300 hover:text-white mt-6 rounded-md text-[15px] w-[100%] relative font-medium -top-1 -left-1 hover:top-0 hover:left-0 transition-all bg-[#FFEB55] hover:bg-[#2b74d4] py-2.5 px-5 uppercase text-black before:content-[''] before:absolute before:top-1 before:left-1 before:hover:top-0 before:hover:left-0 before:w-full before:border-2 before:border-[#FFEB55] before:-z-10 before:transition-all animate__animated animate__fadeInUp animate__delay-2s">
-              Get Started
-            </button>
-            <div className="flex pt-5 items-center justify-center">
-              <hr className="bg-black w-48 h-[2px]" />
-              <p className="px-5">or</p>
-              <hr className="bg-black w-44 h-[2px]" />
+        <div className="w-[50%] flex flex-col items-center justify-center animate__animated animate__fadeInRight pb-72">
+          <div className="">
+            <div className=" pt-28  w-[15%] h-[15%] animate__animated animate__bounceIn ">
+              <Image
+                src="/images/logo1.png"
+                alt="Logo"
+                width={1000}
+                height={1000}
+                className="object-contain"
+              />
             </div>
-            <div className="flex justify-center gap-10">
-              <button className="flex items-center gap-4 font-semibold duration-300 mt-6 rounded-md text-[15px] relative border-2 border-gray-800 bg-transparent py-2.5 px-10 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#4b6cff] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 animate__animated animate__fadeInUp animate__delay-3s">
-                <Image
-                  src="/images/facebook.png"
-                  alt="Facebook"
-                  width={20}
-                  height={20}
-                  className="ml-[-30px]"
-                />
-                Facebook
-              </button>
-              <button className="flex items-center gap-4 font-semibold duration-300 mt-6 rounded-md text-[15px] relative border-2 border-gray-800 bg-transparent py-2.5 px-10 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#000000] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 animate__animated animate__fadeInUp animate__delay-3s">
-                <Image
-                  src="/images/google.png"
-                  alt="Google"
-                  width={20}
-                  height={20}
-                  className="ml-[-30px]"
-                />
-                Google
-              </button>
-            </div>
-            <div className="pt-10">
-              <p className="text-[15px]">
-                Dont have an account?{" "}
-                <a className="text-[#FFEB55] cursor-pointer px-2 animate__animated animate__fadeInUp animate__delay-4s" onClick={() => router.push('/signup')}>Sign Up</a>
+          </div>
+          <div className="flex flex-col items-center">
+            <h1 className="text-[40px] font-bold pt-[140px] animate__animated animate__fadeInDown">
+              Sign In
+            </h1>
+            <h2 className="text-[35px] font-bold pt-5 animate__animated animate__fadeInDown animate__delay-1s">
+              Welcome to PawFund
+            </h2>
+            {authStatus === "failed" && (
+              <p className="text-red-500 animate__animated animate__shakeX">
+                {error}
               </p>
+            )}
+            <div className="w-[50%] pt-[50px]">
+              <Input
+                className="h-[40px] animate__animated animate__fadeInUp"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                style={{ backgroundColor: "#e5e4e4", border: "none" }}
+              />
+              {emailError && <p className="text-red-500">{emailError}</p>}
+              <Input.Password
+                type="password"
+                className="h-[40px] animate__animated animate__fadeInUp animate__delay-1s"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                style={{
+                  backgroundColor: "#e5e4e4",
+                  border: "none",
+                  marginTop: "30px",
+                }}
+              />
+              {passwordError && <p className="text-red-500">{passwordError}</p>}
+              <button
+                onClick={handleSignin}
+                className="font-semibold duration-300 hover:text-white mt-6 rounded-md text-[15px] w-[100%] relative font-medium -top-1 -left-1 hover:top-0 hover:left-0 transition-all bg-[#FFEB55] hover:bg-[#2b74d4] py-2.5 px-5 uppercase text-black before:content-[''] before:absolute before:top-1 before:left-1 before:hover:top-0 before:hover:left-0 before:w-full before:border-2 before:border-[#FFEB55] before:-z-10 before:transition-all animate__animated animate__fadeInUp animate__delay-2s"
+              >
+                Get Started
+              </button>
+              <div className="flex pt-5 items-center justify-center">
+                <hr className="bg-black w-48 h-[2px]" />
+                <p className="px-5">or</p>
+                <hr className="bg-black w-44 h-[2px]" />
+              </div>
+              <div className="flex justify-center gap-10">
+                <button className="flex items-center gap-4 font-semibold duration-300 mt-6 rounded-md text-[15px] relative border-2 border-gray-800 bg-transparent py-2.5 px-10 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#4b6cff] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 animate__animated animate__fadeInUp animate__delay-3s">
+                  <Image
+                    src="/images/facebook.png"
+                    alt="Facebook"
+                    width={20}
+                    height={20}
+                    className="ml-[-30px]"
+                  />
+                  Facebook
+                </button>
+                <button className="flex items-center gap-4 font-semibold duration-300 mt-6 rounded-md text-[15px] relative border-2 border-gray-800 bg-transparent py-2.5 px-10 font-medium uppercase text-gray-800 transition-colors before:absolute before:left-0 before:top-0 before:-z-10 before:h-full before:w-full before:origin-top-left before:scale-x-0 before:bg-[#000000] before:transition-transform before:duration-300 before:content-[''] hover:text-white before:hover:scale-x-100 animate__animated animate__fadeInUp animate__delay-3s">
+                  <Image
+                    src="/images/google.png"
+                    alt="Google"
+                    width={20}
+                    height={20}
+                    className="ml-[-30px]"
+                  />
+                  Google
+                </button>
+              </div>
+              <div className="pt-10">
+                <p className="text-[15px]">
+                  Dont have an account?{" "}
+                  <a
+                    className="text-[#FFEB55] cursor-pointer px-2 animate__animated animate__fadeInUp animate__delay-4s"
+                    onClick={handleSignup}
+                  >
+                    Sign Up
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </Spin>
   );
 };
 
